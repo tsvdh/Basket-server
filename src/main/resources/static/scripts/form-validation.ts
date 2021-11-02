62477098434733d20685aa4c8b40921c84042330
Tobias van den Hurk <T.S.vandenHurk@student.tudelft.nl>
2021-11-02 14:28:34 +0100

    add feedback of entered password

--- src/main/resources/static/scripts/form-validation.ts
+++ src/main/resources/static/scripts/form-validation.ts
@@ -9,10 +9,11 @@ const PASSWORD_PROBLEMS = {
 
 const passwordInput = <HTMLInputElement> document.getElementById("passwordInput");
 
-passwordInput.oninput = function() {
+passwordInput.oninput = function () {
     const password: string = passwordInput.value;
 
-    const faults: string[] = [];
+    /*--- Determine faults in password ---*/
+    let faults: string[] = [];
 
     if (password.length < PASSWORD_LENGTH) {
         faults.push(PASSWORD_PROBLEMS.TOO_SHORT);
@@ -22,5 +23,38 @@ passwordInput.oninput = function() {
         faults.push(PASSWORD_PROBLEMS.NO_NUMBER);
     }
 
-    console.log(faults);
+    /*--- Reset feedback elements and style ---*/
+    passwordInput.classList.remove("is-valid", "is-invalid");
+
+    let feedbackElements: HTMLCollectionOf<Element>
+        = document.getElementsByClassName("password-feedback");
+
+    while (feedbackElements.length > 0) {
+        feedbackElements[0].remove();
+    }
+
+    /*--- Add feedback elements and style ---*/
+    if (faults.length === 0) {
+        passwordInput.classList.add("is-valid");
+    } else {
+        passwordInput.classList.add("is-invalid");
+
+        for (let fault of faults) {
+            let error: HTMLDivElement = document.createElement("div");
+
+            error.classList.add("invalid-feedback");
+            error.classList.add("password-feedback");
+            error.textContent = fault;
+            passwordInput.insertAdjacentElement("afterend", error);
+        }
+    }
+};
+
+
+const usernameInput = <HTMLInputElement> document.getElementById("usernameInput");
+
+usernameInput.oninput = function () {
+    const username: string = usernameInput.value;
+
+    
 };
