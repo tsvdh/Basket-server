d125e9a3e0146aa7bec6dad415ba31338aad2441
Tobias van den Hurk <T.S.vandenHurk@student.tudelft.nl>
2022-04-30 15:51:28 +0200

    Add cancel button

--- src/main/resources/static/scripts/app/releases.ts
+++ src/main/resources/static/scripts/app/releases.ts
@@ -27,16 +27,14 @@ const types = ["icon", "stable", "experimental"];
 
 document.addEventListener("htmx:afterSwap", function () {
     types.forEach((type: string) => {
-        let formId = type + "UploadForm";
-        let progressId = type + "UploadProgress";
+        let form = document.getElementById(type + "UploadForm");
+        let progress = document.getElementById(type + "UploadProgress");
+        let cancelButton = document.getElementById(type + "CancelButton");
 
-        let form = document.getElementById(formId);
         if (form == null) {
             return;
         }
         types.splice(types.indexOf(type), 1);
-
-        let progress = document.getElementById(progressId);
 
         form.addEventListener("htmx:xhr:loadstart", function (event: CustomEvent) {
             form.classList.add("d-none");
@@ -51,7 +49,6 @@ document.addEventListener("htmx:afterSwap", function () {
 
         form.addEventListener("htmx:xhr:loadend", function () {
             progress.classList.add("d-none");
-
             form.classList.remove("d-none");
         });
 
@@ -65,5 +62,12 @@ document.addEventListener("htmx:afterSwap", function () {
             // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-argument
             progress.children.namedItem("text").textContent = `${toMB(event.detail.loaded)} / ${toMB(event.detail.total)} MB`;
         });
+
+        cancelButton.onclick = function () {
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-ignore
+            // eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
+            htmx.trigger(form, "htmx:abort", null);
+        };
     });
 });
