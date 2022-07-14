84e902ea70cab71d6464d2ef3687855da5c5fe64
Tobias van den Hurk <T.S.vandenHurk@student.tudelft.nl>
2022-07-14 12:16:37 +0200

    restyle login

--- src/main/java/basket/server/handlers/AuthHandler.java
+++ src/main/java/basket/server/handlers/AuthHandler.java
@@ -1,27 +1,19 @@
 package basket.server.handlers;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 @Component("authHandler")
 @RequiredArgsConstructor
-public class AuthHandler implements AuthenticationSuccessHandler, AuthenticationFailureHandler {
+public class AuthHandler implements AuthenticationSuccessHandler {
 
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         response.sendRedirect("/users/" + authentication.getName());
-    }
-
-    @Override
-    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-
     }
 }
